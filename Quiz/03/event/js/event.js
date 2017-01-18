f59be0332c39a5416670f29b41fd0622a80d6785
claire0130 <claire10040130@gmail.com>
2017-01-18 14:58:17 +0900

    변수 object로 수정

--- Quiz/03/event/js/event.js
+++ Quiz/03/event/js/event.js
@@ -1,50 +1,47 @@
 (function() {
 	'use strict';
 
-	/**
-	 * --------------------------------
-	 * DOM 탐색 함수
-	 * --------------------------------
-	 */
+	var getDom = {
+		'container'	   : document.querySelector('.container'),
+		'searchBox'    : document.querySelector('.fb-search'),
+		'searchInput'  : document.querySelector('.search-input'),
+		'buttonGroup'  : document.querySelector('.button-group'),
+		'cancelButton' : document.querySelector('.button-cancel'),
+		'message'	   : document.querySelector('.message')
+	}
 
-	var container 	 = document.querySelector('.container');
-	var searchBox 	 = container.querySelector('.fb-search');
-	var searchInput  = searchBox.querySelector('.search-input');
-	var buttonGroup  = searchBox.querySelector('.button-group');
-	var cancelButton = searchBox.querySelector('.button-cancel');
-	var message 	 = container.querySelector('.message');
+	// console.log(getDom.container);
 
 	var eventHandler = {
 		'focus' : function(event) {
 			// var evt = event.target;
-			var buttonGroupHidden = buttonGroup.setAttribute('class', 'hidden');
-			var cancelButtonShow  = cancelButton.setAttribute('class', 'button button-cancel');
+			var buttonGroupHidden = getDom.buttonGroup.setAttribute('class', 'hidden');
+			var cancelButtonShow  = getDom.cancelButton.setAttribute('class', 'button button-cancel');
 			
 			this.buttonGroupHidden;
 			this.cancelButtonShow;
 		},
 		'click' : function(event) {
-			var cancelButtonHidden  = cancelButton.setAttribute('class', 'hidden');
-			var buttonGroupShow  = buttonGroup.setAttribute('class', 'button-group');
+			var cancelButtonHidden  = getDom.cancelButton.setAttribute('class', 'hidden');
+			var buttonGroupShow  	= getDom.buttonGroup.setAttribute('class', 'button-group');
 
 			this.cancelButtonHidden;
 			this.buttonGroupShow;
 		}, 
-		'input' : function(event) {
-			var searchValue = searchInput.value;
-			if ( searchValue.length >= 20 ) {
-				message.setAttribute('class', 'message-show');
+		'input' : function() {
+			var searchValue = getDom.searchInput.value;
+			var searchValueLen = searchValue.length;
+			if ( searchValueLen >= 20 ) {
+				getDom.message.setAttribute('class', 'message-show');
 			}
 
-			if ( searchValue.length <= 9 ) {
-				message.setAttribute('class', 'message-hidden');
+			if ( searchValueLen <= 19 ) {
+				getDom.message.setAttribute('class', 'message-hidden');
 			}
-
 		}
 	};
 
-	searchInput.addEventListener('focus', eventHandler.focus);
-	cancelButton.addEventListener('click', eventHandler.click);
-	searchInput.addEventListener('input', eventHandler.input);
-
+	getDom.searchInput.addEventListener('focus', eventHandler.focus);
+	getDom.cancelButton.addEventListener('click', eventHandler.click);
+	getDom.searchInput.addEventListener('input', eventHandler.input);
 })();
