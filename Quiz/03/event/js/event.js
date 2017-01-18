ab78111b4f36bb328b523b690f9f1ee5d71a04e2
claire0130 <claire10040130@gmail.com>
2017-01-18 13:52:40 +0900

    event  객체로 사용으로 수정

--- Quiz/03/event/js/event.js
+++ Quiz/03/event/js/event.js
@@ -1,6 +1,12 @@
 (function() {
 	'use strict';
 
+	/**
+	 * --------------------------------
+	 * DOM 탐색 함수
+	 * --------------------------------
+	 */
+
 	var container 	 = document.querySelector('.container');
 	var searchBox 	 = container.querySelector('.fb-search');
 	var searchInput  = searchBox.querySelector('.search-input');
@@ -8,30 +14,29 @@
 	var cancelButton = searchBox.querySelector('.button-cancel');
 	var message 	 = container.querySelector('.message');
 
-	// var ERROR_MSG = {
-	// 	'INPUT' : {
-	// 		'EXIST_INPUT' : '10자만 이상 입력이 가능합니다.'
-	// 	}
-	// }
-
 	var eventHandler = {
-		'focus' : function() {
-			buttonGroup.setAttribute('class', 'hidden');
-			cancelButton.setAttribute('class', 'button button-cancel');
+		'focus' : function(event) {
+			// var evt = event.target;
+			var buttonGroupHidden = buttonGroup.setAttribute('class', 'hidden');
+			var cancelButtonShow  = cancelButton.setAttribute('class', 'button button-cancel');
+			
+			this.buttonGroupHidden;
+			this.cancelButtonShow;
 		},
-		'click' : function() {
-			cancelButton.setAttribute('class', 'hidden');
-			buttonGroup.setAttribute('class', 'button-group');
+		'click' : function(event) {
+			var cancelButtonHidden  = cancelButton.setAttribute('class', 'hidden');
+			var buttonGroupShow  = buttonGroup.setAttribute('class', 'button-group');
+
+			this.cancelButtonHidden;
+			this.buttonGroupShow;
 		}, 
-		'input' : function() {
+		'input' : function(event) {
 			var searchValue = searchInput.value;
 			if ( searchValue.length >= 20 ) {
-				// var messageResult = ERROR_MSG.INPUT.EXIST_INPUT;
-				// console.log(messageResult);
 				message.setAttribute('class', 'message-show');
 			}
 
-			if ( searchValue.length <= 19 ) {
+			if ( searchValue.length <= 9 ) {
 				message.setAttribute('class', 'message-hidden');
 			}
 
@@ -41,4 +46,5 @@
 	searchInput.addEventListener('focus', eventHandler.focus);
 	cancelButton.addEventListener('click', eventHandler.click);
 	searchInput.addEventListener('input', eventHandler.input);
+
 })();
